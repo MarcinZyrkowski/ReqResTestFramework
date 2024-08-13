511353298aa67ea2d743dbc4027ce4e624fe3b63
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-08-13 13:47:51 +0200

    added annotations

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.annotation.Full;
 import org.example.client.module.ReqresClient;
 
+@Full
 public class BaseTest {
 
     protected ReqresClient reqresClient = new ReqresClient();
